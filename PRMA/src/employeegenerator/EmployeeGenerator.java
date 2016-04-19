e7c87f225e08bc28135bc4afdd25775038a9e4c9
melihbercin <melihbercin94@gmail.com>
2016-04-19 19:02:40 +0300

    Generator updates

---
+++ PRMA/src/employeegenerator/EmployeeGenerator.java
@@ -0,0 +1,64 @@
+package employeegenerator;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+public class EmployeeGenerator {
+
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = new PrintWriter("staff.txt", "UTF-8");
+		Random randomGenerator = new Random();
+		int taskCounter = 1;
+		writer.println("<employees>");
+		for(int j = 0 ; j < 5; j++)
+		{
+			writer.println("\t<employee>");
+			writer.println("\t\t<employee-id>" + (j+1) + "</employee-id>");
+			if(j == 0){
+				writer.println("\t\t<firstname>Liam</firstname>");
+				writer.println("\t\t<lastname>Miller</lastname>");
+			}
+			else if(j == 1){
+				writer.println("\t\t<firstname>Kyle</firstname>");
+				writer.println("\t\t<lastname>Williams</lastname>");
+			}
+			else if(j == 2){
+				writer.println("\t\t<firstname>Owen</firstname>");
+				writer.println("\t\t<lastname>Taylor</lastname>");
+			}
+			else if(j == 3){
+				writer.println("\t\t<firstname>Max</firstname>");
+				writer.println("\t\t<lastname>Anderson</lastname>");
+			}
+			else if(j == 4){
+				writer.println("\t\t<firstname>Oliver</firstname>");
+				writer.println("\t\t<lastname>Thompson</lastname>");
+			}
+			writer.println("\t\t<depreciation>" + 17.0 + "</depreciation>");
+			
+			int rand = randomGenerator.nextInt(2);
+			if(rand == 0)
+				writer.println("\t\t<workaholism>true</workaholism>");
+			else
+				writer.println("\t\t<workaholism>false</workaholism>");
+			
+			writer.println("\t\t<abilities>\n\t\t\t"
+							+	"<A>" + randomGenerator.nextInt(11) + "</A>\n\t\t\t"
+							+	"<B>" + randomGenerator.nextInt(11) + "</B>\n\t\t\t"
+							+	"<C>" + randomGenerator.nextInt(11) + "</C>\n\t\t\t"
+							+ 	"<D>" + randomGenerator.nextInt(11) + "</D>\n\t\t\t"
+							+	"<E>" + randomGenerator.nextInt(11) + "</E>\n\t\t\t"
+							+	"<F>" + randomGenerator.nextInt(11) + "</F>\n\t\t\t"
+							+	"<G>" + randomGenerator.nextInt(11) + "</G>\n\t\t\t"
+							+	"<H>" + randomGenerator.nextInt(11) + "</H>\n"
+							+ "\t\t</abilities>");
+			writer.println("\t</employee>");
+		}
+		writer.println("</employees>");
+		writer.close();
+	}
+
+}
--- PRMA/src/taskgenerator/TaskGenerator.java
+++ PRMA/src/taskgenerator/TaskGenerator.java
@@ -11,39 +11,41 @@ public class TaskGenerator {
 		PrintWriter writer = new PrintWriter("tasks.txt", "UTF-8");
 		Random randomGenerator = new Random();
 		int taskCounter = 1;
+		writer.println("<projects>");
 		for(int j = 0 ; j < 3; j++)
 		{
-			writer.println("<project>");
-			writer.println("\t<project-id>" + (j + 1) + "</project-id>");
-			writer.println("\t<tasks>");
+			writer.println("\t<project>");
+			writer.println("\t\t<project-id>" + (j + 1) + "</project-id>");
+			writer.println("\t\t<tasks>");
 			for(int i = 0; i < 50; i++)
 			{
-				writer.println("\t\t<task>");
-				writer.println("\t\t\t<task-id>"+(taskCounter)+"</task-id>");
+				writer.println("\t\t\t<task>");
+				writer.println("\t\t\t\t<task-id>"+(taskCounter)+"</task-id>");
 				taskCounter++;
-				writer.println("\t\t\t<name>" + "Task " + (i + 1) + "</name>");
-				writer.println("\t\t\t<duration>" + ((randomGenerator.nextFloat() * 14) + 1) + "</duration>");
+				writer.println("\t\t\t\t<name>" + "Task " + (i + 1) + "</name>");
+				writer.println("\t\t\t\t<duration>" + ((randomGenerator.nextFloat() * 14) + 1) + "</duration>");
 				//writer.println("\t\t\t<knowledge>" + ((randomGenerator.nextFloat() * 9) + 1) + "</knowledge>");
-				writer.println("\t\t\t<startDate> </startDate>");
-				writer.println("\t\t\t<belongsToProjectWithId>" +(j+1) +  "</belongsToProjectWithId>");
-				writer.println("\t\t\t<abilities>\n\t\t\t\t"
-								+	"<A>" + randomGenerator.nextInt(11) + "</A>\n\t\t\t\t"
-								+	"<B>" + randomGenerator.nextInt(11) + "</B>\n\t\t\t\t"
-								+	"<C>" + randomGenerator.nextInt(11) + "</C>\n\t\t\t\t"
-								+ 	"<D>" + randomGenerator.nextInt(11) + "</D>\n\t\t\t\t"
-								+	"<E>" + randomGenerator.nextInt(11) + "</E>\n\t\t\t\t"
-								+	"<F>" + randomGenerator.nextInt(11) + "</F>\n\t\t\t\t"
-								+	"<G>" + randomGenerator.nextInt(11) + "</G>\n\t\t\t\t"
+				writer.println("\t\t\t\t<startDate> </startDate>");
+				writer.println("\t\t\t\t<belongsToProjectWithId>" +(j+1) +  "</belongsToProjectWithId>");
+				writer.println("\t\t\t\t<abilities>\n\t\t\t\t"
+								+	"<A>" + randomGenerator.nextInt(11) + "</A>\n\t\t\t\t\t"
+								+	"<B>" + randomGenerator.nextInt(11) + "</B>\n\t\t\t\t\t"
+								+	"<C>" + randomGenerator.nextInt(11) + "</C>\n\t\t\t\t\t"
+								+ 	"<D>" + randomGenerator.nextInt(11) + "</D>\n\t\t\t\t\t"
+								+	"<E>" + randomGenerator.nextInt(11) + "</E>\n\t\t\t\t\t"
+								+	"<F>" + randomGenerator.nextInt(11) + "</F>\n\t\t\t\t\t"
+								+	"<G>" + randomGenerator.nextInt(11) + "</G>\n\t\t\t\t\t"
 								+	"<H>" + randomGenerator.nextInt(11) + "</H>\n"
-								+ "\t\t\t</abilities>");
-				writer.println("\t\t</task>");
+								+ "\t\t\t\t</abilities>");
+				writer.println("\t\t\t</task>");
 			}
-			writer.println("\t</tasks>");
-			writer.println("\t<priority>" + (j + 1) + "</priority>");
-			writer.println("\t<projectStartDate> </projectStartDate>");
-			writer.println("\t<dueDate> </dueDate>");
-			writer.println("</project>");
+			writer.println("\t\t</tasks>");
+			writer.println("\t\t<priority>" + (j + 1) + "</priority>");
+			writer.println("\t\t<projectStartDate> </projectStartDate>");
+			writer.println("\t\t<dueDate> </dueDate>");
+			writer.println("\t</project>");
 		}
+		writer.println("</projects>");
 		writer.close();
 	}
 }
