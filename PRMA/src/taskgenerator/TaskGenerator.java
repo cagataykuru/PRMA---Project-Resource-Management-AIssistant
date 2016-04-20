8260a3b7a0b8585d4a728e3b71344043c03b876a
Cagatay Kuru <cagatay.kuru@gmail.com>
2016-04-20 13:24:40 +0300

    TaskGenerator düzeltildi

--- PRMA/src/taskgenerator/TaskGenerator.java
+++ PRMA/src/taskgenerator/TaskGenerator.java
@@ -22,11 +22,8 @@ public class TaskGenerator {
 				writer.println("\t\t\t<task>");
 				writer.println("\t\t\t\t<task-id>"+(taskCounter)+"</task-id>");
 				taskCounter++;
-				writer.println("\t\t\t\t<name>" + "Task " + (i + 1) + "</name>");
+				writer.println("\t\t\t\t<name>" + "Task " + taskCounter + "</name>");
 				writer.println("\t\t\t\t<duration>" + ((randomGenerator.nextFloat() * 14) + 1) + "</duration>");
-				writer.println("\t\t\t<name>" + "Task " + (taskCounter) + "</name>");
-				writer.println("\t\t\t<duration>" + ((randomGenerator.nextFloat() * 14) + 1) + "</duration>");
-				//writer.println("\t\t\t<knowledge>" + ((randomGenerator.nextFloat() * 9) + 1) + "</knowledge>");
 				writer.println("\t\t\t\t<startDate> </startDate>");
 				writer.println("\t\t\t\t<belongsToProjectWithId>" +(j+1) +  "</belongsToProjectWithId>");
 				writer.println("\t\t\t\t<abilities>\n\t\t\t\t"
